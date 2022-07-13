c044ffa9c8a169272de66e975acb529b27211a0f
royweb <xytank@gmail.com>
2022-07-13 11:43:58 -0400

    Default Values

---
+++ basic/DefaultValues.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+contract DefaultValues {
+    bool public b;              // false
+    uint public u;              // 0
+    int public i;               // 0
+    address public a;           // 0x0000000000000000000000000000000000000000
+    bytes32 public b32;         // 0x0000000000000000000000000000000000000000000000000000000000000000
+}
+
+// Notes
+// --------------
+// - State variables and local variables in solidity have a default value
